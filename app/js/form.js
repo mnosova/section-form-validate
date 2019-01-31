6cda42916b6b43cf8328f8def53f201a5827db6e
Marina Nosova <nosova_marino4ka@mail.ru>
2019-01-31 22:06:34 +0300

    update input animation,fix autocomplete,add btn style

--- app/js/form.js
+++ app/js/form.js
@@ -60,3 +60,14 @@ $(document).ready(function () {
 
 
 });
+
+//autocomplete fix
+
+if(navigator.userAgent.toLowerCase().indexOf("chrome") >= 0 || navigator.userAgent.toLowerCase().indexOf("safari") >= 0){
+    window.setInterval(function(){
+        $('input:-webkit-autofill').each(function(){
+            var clone = $(this).clone(true, true);
+            $(this).after(clone).remove();
+        });
+    }, 20);
+}
