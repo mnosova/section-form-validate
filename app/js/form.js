fb5fa13c5f2c5d7f47d0bb8068e9caf68d3d4c3c
Marina Nosova <nosova_marino4ka@mail.ru>
2019-02-11 22:04:52 +0300

    refactor BEM naming, add input elements, change animation, add disabled styles

--- app/js/form.js
+++ app/js/form.js
@@ -46,24 +46,51 @@ $(document).ready(function () {
 //input animate
     $('.input__animate').on('keyup', function () {
         if ($(this).val().trim()) {
-            $(this).addClass('input__animate_animated');
-            $(this).siblings('.placeholder__animate').addClass('placeholder__animate_animated');
+            $(this).addClass('input__animate--animated');
+            $(this).siblings('.placeholder__animate').addClass('placeholder__animate--animated');
+            $(this).siblings('.input__show').show();
+            $(this).siblings('.input__clear').show();
+
         }
 
         else {
-            $(this).removeClass('input__animate_animated');
-            $(this).siblings('.placeholder__animate').removeClass('placeholder__animate_animated');
+            $(this).removeClass('input__animate--animated');
+            $(this).siblings('.placeholder__animate').removeClass('placeholder__animate--animated');
+            $(this).siblings('.input__show').hide().addClass('icon-svg--eye-closed').removeClass('icon-svg--eye-opened');
+            $(this).siblings('.input__clear').hide();
 
         }
         return false;
     });
 
+    $('.input__show').on('click', function () {
+        if ($(this).hasClass('icon-svg--eye-closed')) {
+            $(this).siblings('input').attr('type', 'text');
+            $(this).addClass('icon-svg--eye-opened').removeClass('icon-svg--eye-closed');
+        } else {
+            $(this).siblings('input').attr('type', 'password');
+            $(this).addClass('icon-svg--eye-closed').removeClass('icon-svg--eye-opened');
+        }
+        return false;
+    });
+    $('.input__clear').on('click', function () {
+        $(this).siblings('input').val(" ");
+
+        if ($(this).siblings('input').val().trim()) {
+            // $(this).removeClass('hidden');
+        }
+
+        else {
+            // $(this).addClass('hidden');
+        }
+        return false;
+    });
 
 });
 
 //autocomplete fix
 
-if(navigator.userAgent.toLowerCase().indexOf("chrome") >= 0 || navigator.userAgent.toLowerCase().indexOf("safari") >= 0){
+if( navigator.userAgent.match(/(iPod|iPhone|iPad)/)){
     window.setInterval(function(){
         $('input:-webkit-autofill').each(function(){
             var clone = $(this).clone(true, true);
